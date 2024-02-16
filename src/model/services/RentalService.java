ff82ccc49c0e52c6e81f819c3acb68cc1920fca2
Jefferson Duarte <duartejefferson123@gmail.com>
2024-02-16 15:50:39 -0300

    Adicionando Interfaces

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -8,12 +8,12 @@ import java.time.Duration;
 public class RentalService {
     private Double pricePerHour;
     private Double pricePerDay;
-    private BrazilTaxService taxService;
+    private TaxService taxService;
 
-    public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+    public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
         this.pricePerHour = pricePerHour;
         this.pricePerDay = pricePerDay;
-        this.taxService = taxService;
+        this.taxService =  taxService;
     }
 
     public void processInvoice(CarRental carRental) {
